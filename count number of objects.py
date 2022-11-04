c3a0d45fb511b605bf91ae45685c645343864260
MariyaMalanova <79398183+MariyaMalanova@users.noreply.github.com>
2022-11-04 14:11:48 +0800

    Подсчет количества объектов

---
+++ count number of objects.py
@@ -0,0 +1,50 @@
+import numpy as np
+from skimage.measure import label
+from skimage.morphology import binary_erosion
+
+image = np.load("ps.npy.txt")
+labeled = label(image)
+
+mask = np.array([
+                  np.array([
+                            [1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1]]),
+                  np.array([
+                            [1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1],
+                            [1, 1, 0, 0, 1, 1],
+                            [1, 1, 0, 0, 1, 1]]),
+                    np.array([
+                            [1, 1, 0, 0, 1, 1],
+                            [1, 1, 0, 0, 1, 1],
+                            [1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1]]),
+                  np.array([
+                            [1, 1, 1, 1],
+                            [1, 1, 1, 1],
+                            [0, 0, 1, 1],
+                            [0, 0, 1, 1],
+                            [1, 1, 1, 1],
+                            [1, 1, 1, 1]]),
+                  np.array([
+                            [1, 1, 1, 1],
+                            [1, 1, 1, 1],
+                            [1, 1, 0, 0],
+                            [1, 1, 0, 0],
+                            [1, 1, 1, 1],
+                            [1, 1, 1, 1]])],
+                            dtype=object)
+
+all_numbers = 0
+numbers =[0]
+for i in range(0, len(mask)):
+  erosion = binary_erosion(labeled, mask[i])
+  erosion = label(erosion)
+  s = erosion.max()
+  for j in numbers:
+    print("Number of objects: ", s)
+    all_numbers += s
+print("All number of objects:", all_numbers)
+
