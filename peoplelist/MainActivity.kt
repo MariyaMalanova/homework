a68ae22745a125bf7b3f9777030de4391d64fa5a
MariyaMalanova <79398183+MariyaMalanova@users.noreply.github.com>
2022-12-22 17:41:15 +0800

    Add files via upload

---
+++ peoplelist/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.eeeee
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.ListView
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var peopleName : Array<String>
+    lateinit var peopleSurname : Array<String>
+    var mutlist = mutableListOf<String>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val lvPeople = findViewById<ListView>(R.id.people)
+
+        // TODO: СЃРіРµРЅРµСЂРёСЂРѕРІР°С‚СЊ СЃРїРёСЃРѕРє РїРµСЂСЃРѕРЅ РёР· СЃР»СѓС‡Р°Р№РЅС‹С… СЃРѕС‡РµС‚Р°РЅРёР№ РёРјС‘РЅ Рё С„Р°РјРёР»РёР№
+        peopleName = resources.getStringArray(R.array.peopleName)
+        peopleSurname = resources.getStringArray(R.array.peopleSurname)
+
+        for(i in 0 .. 10){
+            val pN = peopleName[Random().nextInt(peopleName.size)]
+            val pS = peopleSurname[Random().nextInt(peopleSurname.size)]
+            mutlist.add("$pN \n$pS")
+        }
+        val adapter = ArrayAdapter<String>(this, R.layout.item, mutlist)
+        lvPeople.adapter = adapter
+    }
+
+    fun onAddPersonClick(view: View) {
+        val nameSurname = findViewById<EditText>(androidx.appcompat.R.id.edit_query)
+        val addNew = nameSurname.text.toString()
+        mutlist.add(addNew)
+    }
+}
