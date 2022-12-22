a983f65c82e2bc9cfeb91f5bf46d9ae507fd81d3
MariyaMalanova <79398183+MariyaMalanova@users.noreply.github.com>
2022-12-22 22:31:03 +0800

    Add files via upload

---
+++ bileti/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.bileti
+
+import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+
+//import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    lateinit var citiesOuts: Array<String>
+    lateinit var citiesIn: Array<String>
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        citiesOuts = resources.getStringArray(R.array.citiesOuts)
+        citiesIn = resources.getStringArray(R.array.citiesIn)
+
+
+        val spinnerOut = findViewById<View>(R.id.citiesOuts) as Spinner;
+        val adapterOut: ArrayAdapter<String> =
+            ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, citiesOuts)
+        adapterOut.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerOut.adapter = adapterOut;
+
+        val spinnerIn = findViewById<View>(R.id.citiesIn) as Spinner;
+        val adapterIn: ArrayAdapter<String> =
+            ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, citiesIn);
+        adapterIn.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerIn.adapter = adapterIn;
+
+    }
+}
